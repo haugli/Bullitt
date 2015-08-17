3e4a6f3ec5d11f7cb91723e62b4b7438db84164c
Chris Haugli <haugli@uber.com>
2015-08-16 21:10:20 -0700

    Replace CommonCrypto with CryptoSwift.

--- Bullitt/String+MD5.swift
+++ Bullitt/String+MD5.swift
@@ -5,22 +5,13 @@
 //  Copyright (c) 2015 Chris Haugli. All rights reserved.
 //
 
-import CommonCrypto
+import CryptoSwift
 
 extension String {
     func MD5() -> String {
         let data = (self as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
-        let result = NSMutableData(length: Int(CC_MD5_DIGEST_LENGTH))!
-        let resultBytes = UnsafeMutablePointer<CUnsignedChar>(result.mutableBytes)
-        CC_MD5(data.bytes, CC_LONG(data.length), resultBytes)
+        let hashData = CryptoSwift.Hash.md5(data).calculate()!
         
-        let a = UnsafeBufferPointer<CUnsignedChar>(start: resultBytes, count: result.length)
-        let hash = NSMutableString()
-        
-        for i in a {
-            hash.appendFormat("%02x", i)
-        }
-        
-        return hash as String
+        return hashData.toHexString()
     }
 }
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ platform :ios, '8.0'
 use_frameworks!
 
 pod 'Alamofire', '~> 1.3'
+pod 'CryptoSwift', '~> 0.0.13'
 pod 'SwiftyJSON', '~> 2.2.1'
